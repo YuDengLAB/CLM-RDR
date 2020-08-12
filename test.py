d7b5c418a48aac482e5dea8c6ce83ad5975351f2
YuDengLAB <52067451+YuDengLAB@users.noreply.github.com>
2020-08-12 22:46:48 +0800

    Update test.py
    v2 update the test module to enable processing of numerical features

--- test.py
+++ test.py
@@ -2,14 +2,13 @@ import tensorflow as tf
 import os
 import sys
 import numpy as np
+from conf import config
 sys.path.append("./conf")
-import config
 from data_utils import Data
-from deepedr import EDRConvNet
 if __name__ == '__main__':
     sess = tf.Session()
-    saver = tf.train.import_meta_graph('./runs/1559730669/checkpoints/model-16400.meta')
-    saver.restore(sess, tf.train.latest_checkpoint('./runs/1559730669/checkpoints/'))
+    saver = tf.train.import_meta_graph('./runs/1597229589/checkpoints/model-780.meta')
+    saver.restore(sess, tf.train.latest_checkpoint('./runs/1597229589/checkpoints/'))
 
     test_data = Data(data_source=config.test_data_source,
                           alphabet=config.alphabet,
@@ -17,18 +16,12 @@ if __name__ == '__main__':
                           batch_size=config.batch_size,
                           no_of_classes=config.no_of_classes)
     test_data.load_Test_Data()
-
     graph = tf.get_default_graph()
-    # for op in graph.get_operations():
-    #     print(op.name)
     x_ = graph.get_tensor_by_name("Input-Layer/input_x:0")
     keep_prob_ = graph.get_tensor_by_name("Input-Layer/dropout_keep_prob:0")
-    pred_test = graph.get_tensor_by_name("OutputLayer/score:0")
+    pred_test = graph.get_tensor_by_name("OutputLayer/scores:0")
     xin = test_data.test_get()
     feed_dict = {x_: xin, keep_prob_: 1.0}
     pred = sess.run([pred_test], feed_dict)
-    pred_ = []
-    for i in pred:
-        index = np.argmax(i)
-        pred_.append(index)
-    print(pred_)
+    label = np.argmax(pred[0], axis=1)
+    print(label)
